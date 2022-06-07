262e5131af51ff5c1eb2a8c3d05268bf845bad2d
Leandro Paiva <leandroapaiva@yahoo.com>
2022-06-07 19:58:24 -0300

    01 em andamento

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,23 +1,40 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function Table() {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
+    const fetchTable = async () => {
+      const response = await fetch(
+        'https://swapi-trybe.herokuapp.com/api/planets/',
+      );
       const data = await response.json();
-      console.log(data);
+      /* console.log(data); */
+      setData(data.results);
     };
-    fetchData();
+    fetchTable();
   }, []);
   return (
     <div>
       <table>
-        
+        <thead>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </thead>
       </table>
     </div>
   );
 }
 
 export default Table;
-
-// https://swapi-trybe.herokuapp.com/api/planets/
